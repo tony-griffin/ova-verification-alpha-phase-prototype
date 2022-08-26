375e82e085d2d94c75d4e7588ea7fdf4e25656cf
Tony <tony@madetech.com>
2022-08-26 13:23:02 +0100

    WIP: sort user names by most likely year

--- app/assets/javascripts/getClaimName.js
+++ app/assets/javascripts/getClaimName.js
@@ -1,10 +1,11 @@
 function getClaimNames(getFakeDIClaimResponse) {
-  const fakeJWT = getFakeDIClaimResponse;
-  const claimNames = fakeJWT.vc.credentialSubject.name;
+  const claimNames = getFakeDIClaimResponse.vc.credentialSubject.name;
 
   let currentName = [];
   let previousNames = [];
+  let nameAtDischarge = [];
   let namesExport = [];
+  
 
   claimNames.map((name) => {
     if (name.validFrom) {
@@ -15,6 +16,7 @@ function getClaimNames(getFakeDIClaimResponse) {
 
     if (name.validUntil) {
       name.nameParts.map((namePart) => {
+        
         previousNames.push(namePart.value);
       });
     }
@@ -23,8 +25,45 @@ function getClaimNames(getFakeDIClaimResponse) {
   namesExport.push(currentName.join(" "));
   namesExport.push(previousNames.join(" "));
 
-  console.log("Names array!!!!!: ", namesExport);
   return namesExport;
+}
+
+function getNameAtDischarge(validUntilDate) {
+  let matchDistance = {};
+  matchDistance.validUntil = validUntilDate;
+
+  let validUntilArray = matchDistance.validUntil.split("-");
+  let validUntilYear = Number(validUntilArray[0]);
+  let yearArray = [];
+  yearArray.push(dischargeYear);
+  yearArray.push(validUntilYear);
+  //descending order
+  yearArray.sort((a, b) => b - a);
+  let yearDiff = yearArray[0] - yearArray[1];
+
+  matchDistance.yearDiff = yearDiff;
+  distanceFromDischargeYear.push(matchDistance);
+  let uniqueDateValues = [
+    ...new Set(distanceFromDischargeYear.map((el) => JSON.stringify(el))),
+  ].map((el) => JSON.parse(el));
+
+  //ascending order
+  uniqueDateValues.sort(
+    (objA, objB) => Number(objA.yearDiff) - Number(objB.yearDiff)
+  );
+  console.log("UNIQUE--------", uniqueDateValues);
+  let mostLikelyYear = Number(uniqueDateValues[0].validUntil.split("-")[0]);
+  console.log(mostLikelyYear);
+
+  let upperLikelyRange = dischargeYear + 5;
+  let lowerLikelyRange = dischargeYear - 5;
+
+  if (
+    mostLikelyYear === dischargeYear ||
+    mostLikelyYear <= upperLikelyRange ||
+    mostLikelyYear >= lowerLikelyRange
+  ) {
+  }
 }
 
 module.exports = {
