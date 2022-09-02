61e156df2644ca6df52816ba4b607bf91639d9ce
Tony <tony@madetech.com>
2022-09-02 14:39:23 +0100

    Run lint standard fix

--- app/assets/javascripts/fakeDIClaimJWT.js
+++ app/assets/javascripts/fakeDIClaimJWT.js
@@ -1,83 +1,83 @@
-function getFakeDIClaimResponse(birthYear) {
-  let birthYearNumber = Number(birthYear);
+function getFakeDIClaimResponse (birthYear) {
+  const birthYearNumber = Number(birthYear)
 
-  let fakeJWT = {
-    sub: "urn:fdc:gov.uk:2022:56P4CMsGh_02YOlWpd8PAOI-2sVlB2nsNU7mcLZYhYw=",
-    iss: "https://identity.integration.account.gov.uk/",
+  const fakeJWT = {
+    sub: 'urn:fdc:gov.uk:2022:56P4CMsGh_02YOlWpd8PAOI-2sVlB2nsNU7mcLZYhYw=',
+    iss: 'https://identity.integration.account.gov.uk/',
     nbf: 1541493724,
     iat: 1541493724,
     exp: 1573029723,
-    vot: "P2",
-    vtm: "https://oidc.integration.account.gov.uk/trustmark",
+    vot: 'P2',
+    vtm: 'https://oidc.integration.account.gov.uk/trustmark',
     vc: {
-      type: ["VerifiableCredential", "VerifiableIdentityCredential"],
+      type: ['VerifiableCredential', 'VerifiableIdentityCredential'],
       credentialSubject: {
         name: [
           {
-            validFrom: "2020-03-01",
+            validFrom: '2020-03-01',
             nameParts: [
               {
-                value: "Sandy",
-                type: "GivenName",
+                value: 'Sandy',
+                type: 'GivenName'
               },
               {
-                value: "Smith",
-                type: "FamilyName",
-              },
-            ],
+                value: 'Smith',
+                type: 'FamilyName'
+              }
+            ]
           },
           {
-            validUntil: "2020-03-01",
+            validUntil: '2020-03-01',
             nameParts: [
               {
-                value: "Sandy",
-                type: "GivenName",
+                value: 'Sandy',
+                type: 'GivenName'
               },
               {
-                value: "Williams",
-                type: "FamilyName",
-              },
-            ],
+                value: 'Williams',
+                type: 'FamilyName'
+              }
+            ]
           },
           {
-            validFrom: "1999-06-05",
+            validFrom: '1999-06-05',
             nameParts: [
               {
-                value: "Sandy",
-                type: "GivenName",
+                value: 'Sandy',
+                type: 'GivenName'
               },
               {
-                value: "Williams",
-                type: "FamilyName",
-              },
-            ],
+                value: 'Williams',
+                type: 'FamilyName'
+              }
+            ]
           },
           {
-            validUntil: "1999-06-05",
+            validUntil: '1999-06-05',
             nameParts: [
               {
-                value: "Sandy",
-                type: "GivenName",
+                value: 'Sandy',
+                type: 'GivenName'
               },
               {
-                value: "Murphy",
-                type: "FamilyName",
-              },
-            ],
-          },
+                value: 'Murphy',
+                type: 'FamilyName'
+              }
+            ]
+          }
         ],
         birthDate: [
           {
-            value: `${birthYearNumber}-01-01`,
-          },
-        ],
-      },
-    },
-  };
+            value: `${birthYearNumber}-01-01`
+          }
+        ]
+      }
+    }
+  }
 
-  return fakeJWT;
+  return fakeJWT
 }
 
 module.exports = {
-  getFakeDIClaimResponse,
-};
+  getFakeDIClaimResponse
+}
